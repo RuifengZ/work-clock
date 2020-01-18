c433ea8fae6d3e8a2ea37c183693e31f0246eee1
RuifengZ <rzhang@dealersocket.com>
2020-01-17 17:08:26 -0800

    added formgroup

--- src/components/TimeSheet.js
+++ src/components/TimeSheet.js
@@ -1,15 +1,21 @@
-import React from "react";
-import ListGroup from "react-bootstrap/ListGroup";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 export default function TimeSheet() {
+  const [clockIns, setclockIns] = useState(initialState);
+
   return (
     <div>
-      <ListGroup>
-        <ListGroup.Item disabled>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-      </ListGroup>
+      <Form>
+        <Form.Group controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="time" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Form>
     </div>
   );
 }
